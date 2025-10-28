d00b0431c13439798c4220da350160c0ad668243
JD <ellidarel@gmail.com>
2025-10-28 18:34:47 +0800

    UpdatE

--- functions/server.js
+++ functions/server.js
@@ -35,7 +35,7 @@ app.use(
     optionsSuccessStatus: 204,
   })
 );
-app.options('*', cors());
+app.options('/*', cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
